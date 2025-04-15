de3fb77b2a4424d2af3dcb5700ec21ece10a46d0
JeufoDev <llucasjefferson76@gmail.com>
2025-04-15 15:48:48 -0300

    Modificação

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const usuarios = require('./data/usuarios');
+const usuarios = require('../data/usuarios');
 
 app.use(express.json());
 
